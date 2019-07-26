0eb9914b91b5fff8e9b0d94de188a56335e14872
Mihai <mihaisaftas.259@gmail.com>
2019-07-26 15:41:04 +0300

    problema n4

---
+++ src/main/java/ProblemeJava/MadLib.java
@@ -0,0 +1,18 @@
+package ProblemeJava;
+
+import java.util.Scanner;
+
+public class MadLib {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a noun:");
+        String noun = scanner.nextLine();
+        System.out.println("Enter a verb:");
+        String verb = scanner.nextLine();
+        System.out.println("Enter an adjective");
+        String adjective = scanner.nextLine();
+        System.out.println("Enter an adverb:");
+        String adverb = scanner.nextLine();
+        System.out.println("Do you" + " " + verb + " " + "your" + " " + adjective + " " + noun + " " + " " +adverb + "?");
+    }
+}
