f3851e06c74760e0e2b3900be01dffe9019c62bb
Mihai <mihaisaftas.259@gmail.com>
2019-07-26 15:34:49 +0300

    problema n3

--- src/main/java/ProblemeJava/CountingNumberOfCharacters.java
+++ src/main/java/ProblemeJava/CountingNumberOfCharacters.java
@@ -1,18 +1,18 @@
-package ProblemeJava;
-
-import java.util.Scanner;
-
-public class CountingNumberOfCharacters {
-
-        public static void main(String args[]){
-            System.out.printf("What is the input string?");
-            Scanner scanner = new Scanner(System.in);
-            String text = scanner.nextLine();
-            if (text.length() > 0);{
-                System.out.printf(text + " " + "has" + " " + text.length() + " " + "characters.");
-            }
-if(text.length() == 0){
-    System.out.printf("Please enter at least one character!");
-}
-        }}
-
+//package ProblemeJava;
+//
+//import java.util.Scanner;
+//
+//public class CountingNumberOfCharacters {
+//
+//        public static void main(String args[]){
+//            System.out.printf("What is the input string?");
+//            Scanner scanner = new Scanner(System.in);
+//            String text = scanner.nextLine();
+//            if (text.length() > 0);{
+//                System.out.printf(text + " " + "has" + " " + text.length() + " " + "characters.");
+//            }
+//if(text.length() == 0){
+//    System.out.printf("Please enter at least one character!");
+//}
+//        }}
+//
---
+++ src/main/java/ProblemeJava/PrintingQuotes.java
@@ -0,0 +1,14 @@
+//package ProblemeJava;
+//
+//import java.util.Scanner;
+//
+//public class PrintingQuotes {
+//    public static void main(String[] args) {
+//        System.out.println("What is the quote?");
+//        Scanner scanner = new Scanner(System.in);
+//        String quote = scanner.nextLine();
+//        System.out.println("Who said it?");
+//        String author = scanner.nextLine();
+//        System.out.println("Obi-Wan Kenobi says, \"These aren`t the droids you`re looking for.\"");
+//    }}
+//
